b1a76960a58e7cb677fa3e27dcac10b433e409a0
ron-oren97 <129007239+ron-oren97@users.noreply.github.com>
2023-05-07 22:06:38 +0300

    Fixed run tests script

--- ex1/runTests.sh
+++ ex1/runTests.sh
@@ -13,7 +13,7 @@ export TMPDIR="$PWD/tmpdir"
 
 for i in {0..10}
 do
-   valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=./Tests/valgrind-out$i.txt ./HackEnrollment -i ./Test$i\ */students.txt ./Test$i\ */courses.txt ./Test$i\ */hackers.txt ./Test$i\ */queues.txt ./Test$i\ */out.txt
+   # valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=./Tests/valgrind-out$i.txt ./HackEnrollment -i ./Test$i\ */students.txt ./Test$i\ */courses.txt ./Test$i\ */hackers.txt ./Test$i\ */queues.txt ./Test$i\ */out.txt
    if [[ `valgrind --leak-check=full ./HackEnrollment -i ./Tests/Test$i\ */students.txt ./Tests/Test$i\ */courses.txt ./Tests/Test$i\ */hackers.txt ./Tests/Test$i\ */queues.txt ./Tests/Test$i\ */out.txt |& grep 'All heap blocks were freed -- no leaks are possible'` == "" ]]; then
 	echo -e -n "Test number $i result: ${RED}leaked memory${NC}"
    else
